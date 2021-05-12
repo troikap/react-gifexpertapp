788de009dff2da147918dc062b98d477266a7206
Troikap <lucasperezgracia@gmail.com>
2021-05-11 21:21:06 -0300

    subiendo 04

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+
+export const getGifs = async ( category ) => {
+  const apiKey = 'yyxlu7IzYd9JQzoCBCm5Pl93L2y7haSe'
+  const peticion = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ encodeURI( category ) }&limit=5`)
+  const { data } = await peticion.json();
+  const gifs = data.map( ( gif ) => {
+    return {
+      id: gif.id,
+      title: gif.title,
+      url: gif.images?.downsized_medium.url
+    }
+  })
+  return gifs;
+}
