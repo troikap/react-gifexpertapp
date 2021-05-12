f7b548dfa3337536f0bd76a389bdeda8f9c1494a
Troikap <lucasperezgracia@gmail.com>
2021-05-11 21:36:13 -0300

    cambio http a https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async ( category ) => {
   const apiKey = 'yyxlu7IzYd9JQzoCBCm5Pl93L2y7haSe'
-  const peticion = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ encodeURI( category ) }&limit=5`)
+  const peticion = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ encodeURI( category ) }&limit=5`)
   const { data } = await peticion.json();
   const gifs = data.map( ( gif ) => {
     return {
